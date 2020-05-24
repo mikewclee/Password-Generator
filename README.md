d461b628d1e30f46c47c871599e93a11d6a29b8f
Michael W Lee <mikewclee@gmail.com>
2020-05-23 20:37:40 -0400

    Initial password generator codes

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# Password-Generator
+# 03 JavaScript: Password Generator
+
+This application generates a random password based on below user-selected criteria:
+* length of at least 8 characters and no more than 128 characters
+* lowercase, uppercase, numeric, and/or special characters
+
+. This app will run in the browser and feature dynamically updated HTML and CSS powered by your JavaScript code. It will also feature a clean and polished user interface and be responsive, ensuring that it adapts to multiple screen sizes.
+
+
---
+++ assets/javascript/scripts.js
@@ -0,0 +1,75 @@
+// Assignment Code to associate the generate id with the button
+var generateButton = document.querySelector("#generate");
+
+
+var passwordNew = document.getElementById("password");
+passwordNew.innerHTML = generatePassword();
+
+function generatePassword() {
+    // declare variables 
+    var lowers = "abcdefghijklmnopqrstuvwxyz";
+    var uppers = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var numerics = "0123456789";
+    var specials = "!@#$%^&*()_+";
+    var charLength = "";
+    var finalPassword = '';
+    
+    // WHEN prompted for the length of the password THEN I choose a length of at least 8 characters and no more than 128 characters
+    var charLength = prompt("Please enter password length between 8 characters and 128 characters:");
+    // check user input if it's betw 8 to 128 characters
+    while (charLength < 8 || charLength > 128) {
+        var charLength = prompt("Password length must be a value between 8 to 128.")
+    }
+    
+    // WHEN prompted for character types to include in the password THEN I choose lowercase, uppercase, numeric, and/or special characters
+    // WHEN I answer each prompt THEN my input should be validated and at least one character type should be selected
+    var confirmLowers = confirm("Click OK to include lower case characters in password.");
+    var confirmUppers = confirm("Click OK to include upper case characters in password.");
+    var confirmNumerics = confirm("Click OK to include numerical characters in password.");
+    var confirmSpecials = confirm("Click OK to include special characters in password.");
+    
+    // THEN my input should be validated and at least one character type should be selected
+    while (confirmLowers === false && confirmUppers === false && confirmNumberics === false && confirmSpecials === false) {
+        alert("Must choose at lease one parameter");
+        var confirmLowers = confirm("Click OK to include lower case characters in password.");
+        var confirmUppers = confirm("Click OK to include upper case characters in password.");
+        var confirmNumerics = confirm("Click OK to include numerical characters in password.");
+        var confirmSpecials = confirm("Click OK to include special characters in password.");
+    }
+    
+    // build password string depending on user input
+    var passwordStr ='';
+    
+    if (confirmLowers == true) {
+        passwordStr += lowers;
+        console.log(passwordStr);
+    }
+    if (confirmUppers == true) {
+        passwordStr += uppers;
+        console.log(passwordStr);
+    }
+    if (confirmNumerics == true) {
+        passwordStr += numerics;
+        console.log(passwordStr);
+    }
+    if (confirmSpecials == true) {
+        passwordStr += specials;
+        console.log(passwordStr);
+    }
+    console.log(passwordStr);
+    // create for loop to choose password characters 
+    var finalPassword ='';
+    
+    for (var i = 0; i < charLength; i++) {
+        var char = Math.floor(Math.random() * passwordStr.length);
+        finalPassword += passwordStr.charAt(char);
+        console.log(finalPassword)
+    }
+    return finalPassword;
+    
+    // document.getElementById("password").value = finalPassword;
+}
+
+// Add event listener to generate button
+generateButton.addEventListener("click", generatePassword)
+
